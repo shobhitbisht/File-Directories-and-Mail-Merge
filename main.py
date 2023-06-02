46cc0d5b8c38881eea592ef26c9f655597691ccf
shobhitbisht <73101799+shobhitbisht@users.noreply.github.com>
2023-06-02 19:32:58 +0000

    short solution or say optimized solution

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+PLACEHOLDER = "[name]"
+
 with open("./names/invited_names.txt", mode = 'r') as file:
-  namelist = file.readlines()
-  print(namelist)
-for i in range(len(namelist)):
-  with open("./input/letters/startingletter.txt", mode = 'r') as file:
-    letterfirstline = file.readlines(1)
-    letterlines = file.readlines()
-    print(letterfirstline)
-    print(letterlines)
-    str = letterfirstline[0]
-    print(str)
-    
-    up_str = str.replace("[name]",f"{namelist[i].strip()}")
-    print(up_str)
-    
+  names = file.readlines()
+  print(names)
   
-  with open(f"./output/readytosend/letter{namelist[i]}.txt", mode = "w") as file:
-    file.write(f"{up_str}")
-    for j in letterlines:
-      file.write(j)
+
+  
+with open("./input/letters/startingletter.txt", mode = 'r') as letter_file:
+  letter_contents = letter_file.read()
+  
+  for name in names:
+    name = name.strip()
+    new_letter = letter_contents.replace(PLACEHOLDER, name)
+    print(new_letter)
+    
+    with open(f"./output/readytosend/letter{name}.txt", mode = "w") as file:
+      file.write(new_letter)
+    
+    
   
   
